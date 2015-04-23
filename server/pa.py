463a119517b9533ae244c3295964e167b17177fb
Yuxiang Zhou <yz4009@antony.doc.ic.ac.uk>
2015-04-23 18:15:18 +0100

    Restructure

--- server/pa.py
+++ server/pa.py
@@ -2,6 +2,7 @@ import sys
 import os
 import Queue
 import threading
+import logging
 from test_module import TestModule
 from AutoSign.sign import ACGSignModule
 
@@ -14,14 +15,16 @@ def add_Module(q, am):
 def execute_queue(q, thread_list):
     while True:
         msg_obj = q.get()
-        os.system('notify-send -u critical -t 3000 -i {} "{}" "{}"'.format(
-            msg_obj['icon'],
-            msg_obj['title'],
-            msg_obj['content'])
-        )
+        # os.system('notify-send -u critical -t 3000 -i {} "{}" "{}"'.format(
+        #     msg_obj['icon'],
+        #     msg_obj['title'],
+        #     msg_obj['content'])
+        # )
+        logging.info(msg_obj)
         q.task_done()
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='/var/log/pa/server_info.log', level=logging.DEBUG)
     assist_modules = [TestModule, ACGSignModule]
     q = Queue.Queue(maxsize=0)
     thread_list = []
